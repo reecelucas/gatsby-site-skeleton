e7d8678fcf1fef2bb0bba0d6fc633ecc805b1ec5
Reece Lucas <reecelucas@sky.com>
2018-05-29 09:23:46 +0100

    Small formatting changes

--- src/components/Accordion/AccordionItemContext.tsx
+++ src/components/Accordion/AccordionItemContext.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 
-const AccordionItemContext: React.Context<any> = React.createContext();
+const AccordionItemContext: React.Context<any> = React.createContext({});
 
 export default AccordionItemContext;
